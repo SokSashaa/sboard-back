d41d7f11cf2931a490b7bd30153b19506dac54be
SokSashaa <127789980+SokSashaa@users.noreply.github.com>
2024-10-21 13:21:03 +0300

    add summary in poll controller

--- src/polls/polls.controller.ts
+++ src/polls/polls.controller.ts
@@ -1,30 +1,35 @@
-import {Body, Controller, Delete, Get, Param, Post, UsePipes, ValidationPipe} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post} from '@nestjs/common';
 import {PollsService} from './polls.service';
 import {CreatePollDto} from './dto/create-poll.dto';
-import {ApiBody} from "@nestjs/swagger";
+import {ApiOperation, ApiTags} from "@nestjs/swagger";
 import {CreateVoteDto} from "./dto/create-vote.dto";
 
 @Controller('polls')
+@ApiTags('Polls')
 export class PollsController {
     constructor(private readonly pollsService: PollsService) {
     }
 
     @Post(':id/vote')
+    @ApiOperation({summary: 'Голос за определенный ответ в определенном голосовании'})
     vote(@Param('id') id: number, @Body() data: CreateVoteDto) {
         return this.pollsService.vote(id, data)
     }
 
     @Post()
+    @ApiOperation({summary: 'Создание голосования'})
     create(@Body() data: CreatePollDto) {
         return this.pollsService.create(data)
     }
 
     @Get()
+    @ApiOperation({summary: 'Получение всех голосований'})
     findAll() {
         return this.pollsService.getAllPolls()
     }
 
     @Delete(':id')
+    @ApiOperation({summary: 'Удаление голосования по его id '})
     remove(@Param('id') id: string) {
         return this.pollsService.deletePolls(id)
     }
